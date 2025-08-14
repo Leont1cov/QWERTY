d839fe94ae9dd25c79f0042a827e521ec13c38e4
Leont1cov <chuchunovvakov@mail.ru>
2025-08-14 12:51:12 +0300

    chore: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "./components/layout/sidebar/Sidebar"
+import PostPublication from "./components/PostPublication"
+
+export default function App() {
+    return (
+        <main className="main">
+            <Sidebar />
+            <PostPublication />
+        </main>
+    )
+}
