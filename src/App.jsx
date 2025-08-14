09f9b7e57fe5804aea9b80b1f976442eff5ce865
Leont1cov <chuchunovvakov@mail.ru>
2025-08-14 21:24:15 +0300

    feat: added header on the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Sidebar from "./components/layout/sidebar/Sidebar"
-import PostPublication from "./components/PostPublication"
+import Header from "./components/layout/header/Header";
+import Sidebar from "./components/layout/sidebar/Sidebar";
+import PostPublication from "./components/PostPublication";
 
 export default function App() {
-    return (
-        <main className="main">
-            <Sidebar />
-            <PostPublication />
-        </main>
-    )
+  return (
+    <main className="main">
+      <Header />
+      <Sidebar />
+      <PostPublication />
+    </main>
+  );
 }
