54aa30e007954987203d45f791c53eabc1144ab0
Rodion Kreimer <kreimerrodion@gmail.com>
2025-08-16 16:00:05 +0300

    style: update post-publication's location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/layout/header/Header"
 import Sidebar from "./components/layout/sidebar/Sidebar"
-import PostPublication from "./components/PostPublication"
+import PostPublication from "./components/layout/post-publication/PostPublication"
 
 export default function App() {
     return (
---
+++ src/components/layout/post-publication/PostPublication.jsx
@@ -0,0 +1,38 @@
+import Button from "../../ui/Button"
+export default function PostPublication({ profileLink, profilePicture }) {
+    return (
+        <div className="post">
+            <a href={profileLink} className="post__picture">
+                {/* <img
+                    src={profilePicture}
+                    alt="Profile picture"
+                    className="post__picture--profilePicture"
+                /> */}
+                <div className="post__picture--profilePicture"></div>
+                {/* ^^^^^ Change this to the img element when profile picture data will be connected to the component */}
+            </a>
+
+            <div className="post__right">
+                <textarea
+                    className="post__right--textarea"
+                    name="input"
+                    id="input"
+                    placeholder="What's happening?"
+                ></textarea>
+
+                <div className="post__bottom">
+                    <i
+                        class="post__bottom--imageBtn fa fa-picture-o"
+                        aria-hidden="true"
+                    ></i>
+
+                    <Button
+                        title={"Post"}
+                        buttonClass="button button__PostPublication"
+                        button__titleClass="button__title button__title-PostPublication"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
