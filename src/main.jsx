9ecccb6a31510f478f63e8c6fd3b9378f39bb14c
Leont1cov <chuchunovvakov@mail.ru>
2025-08-16 19:11:43 +0300

    chore: install react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles/App.scss";
 
-createRoot(document.getElementById("root")).render(<App />);
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
