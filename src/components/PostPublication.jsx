f1ea39ff08a6c749df30f8d84a68749731d7582a
Leont1cov <chuchunovvakov@mail.ru>
2025-08-16 12:30:16 +0300

    chore: add fixes

--- src/components/PostPublication.jsx
+++ src/components/PostPublication.jsx
@@ -1,39 +1,39 @@
-import Button from "./ui/Button"
-
+import Button from "./ui/Button";
+// ! Так и не переместил компонент в папку layout
 export default function PostPublication({ profileLink, profilePicture }) {
-    return (
-        <div className="post">
-            <a href={profileLink} className="post__picture">
-                {/* <img
+  return (
+    <div className="post">
+      <a href={profileLink} className="post__picture">
+        {/* <img
                     src={profilePicture}
                     alt="Profile picture"
                     className="post__picture--profilePicture"
                 /> */}
-                <div className="post__picture--profilePicture"></div>
-                {/* ^^^^^ Change this to the img element when profile picture data will be connected to the component */}
-            </a>
+        <div className="post__picture--profilePicture"></div>
+        {/* ^^^^^ Change this to the img element when profile picture data will be connected to the component */}
+      </a>
 
-            <div className="postRight">
-                <textarea
-                    className="postRight__textarea"
-                    name="input"
-                    id="input"
-                    placeholder="What's happening?"
-                ></textarea>
+      <div className="postRight">
+        <textarea
+          className="postRight__textarea"
+          name="input"
+          id="input"
+          placeholder="What's happening?"
+        ></textarea>
 
-                <div className="postBottom">
-                    <i
-                        class="postBottom__imageBtn fa fa-picture-o"
-                        aria-hidden="true"
-                    ></i>
+        <div className="postBottom">
+          <i
+            class="postBottom__imageBtn fa fa-picture-o"
+            aria-hidden="true"
+          ></i>
 
-                    <Button
-                        title={"Post"}
-                        buttonClass="button button__PostPublication"
-                        button__titleClass="button__title button__title-PostPublication"
-                    />
-                </div>
-            </div>
+          <Button
+            title={"Post"}
+            buttonClass="button button__PostPublication"
+            button__titleClass="button__title button__title-PostPublication"
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
