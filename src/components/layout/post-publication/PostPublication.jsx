9840f99fc0f313e7d7356eb4d951c0d6243cf8aa
Rodion Kreimer <kreimerrodion@gmail.com>
2025-08-18 18:09:41 +0300

    feat: add multi-text compatibility to the post publication component

--- src/components/layout/post-publication/PostPublication.jsx
+++ src/components/layout/post-publication/PostPublication.jsx
@@ -1,7 +1,20 @@
+import { useRef } from "react"
+
 import Button from "../../ui/Button"
 import ProfilePicture from "../../ui/ProfilePicture"
 
 export default function PostPublication({ profilePic, profileLink }) {
+    const textareaRef = useRef(null)
+
+    function handleInput() {
+        const el = textareaRef.current
+
+        if (el) {
+            el.style.height = "auto"
+            el.style.height = el.scrollHeight + "px"
+        }
+    }
+
     return (
         <div className="postPublication">
             <a href={profileLink} className="postPublication__picture">
@@ -9,10 +22,13 @@ export default function PostPublication({ profilePic, profileLink }) {
             </a>
             <div className="postPublication__right">
                 <textarea
+                    ref={textareaRef}
                     className="postPublication__right--textarea"
                     name="input"
                     id="input"
                     placeholder="What's happening?"
+                    onInput={handleInput}
+                    rows={1}
                 ></textarea>
 
                 <div className="postPublication__bottom">
