7a9cf2a7c93dd1914b9180c69d942c28d375388c
Rodion Kreimer <kreimerrodion@gmail.com>
2025-08-18 18:07:45 +0300

    fix: update database

---
+++ src/data/userData.js
@@ -0,0 +1,19 @@
+import profilePicture from "../assets/profilePicture1.JPG"
+import profilePicture2 from "../assets/profilePicture2.JPG"
+
+const userData = {
+    profile: {
+        profilePic: profilePicture,
+        username: "John Smith",
+        tag: "@johnsmith",
+    },
+    followings: [
+        {
+            profilePic: profilePicture2,
+            username: "Michael Brown",
+            tag: "@michaelbrown",
+        },
+    ],
+}
+
+export default userData
