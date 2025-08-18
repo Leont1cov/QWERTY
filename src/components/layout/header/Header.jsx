70c24e1cdfe8312ec875b9ca26565c16a9909758
Leont1cov <chuchunovvakov@mail.ru>
2025-08-18 18:47:25 +0300

    merge

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,23 +1,23 @@
 export default function Header({ currentSection, setCurrentSection }) {
-    return (
-        <header className="header">
-            <button
-                onClick={() => setCurrentSection("For you")}
-                className={`header__tab ${
-                    currentSection === "For you" ? "header__tab-active" : ""
-                }`}
-            >
-                For you
-            </button>
+  return (
+    <header className="header">
+      <button
+        onClick={() => setCurrentSection("For you")}
+        className={`header__tab ${
+          currentSection === "For you" ? "header__tab-active" : ""
+        }`}
+      >
+        For you
+      </button>
 
-            <button
-                onClick={() => setCurrentSection("Following")}
-                className={`header__tab ${
-                    currentSection === "Following" ? "header__tab-active" : ""
-                }`}
-            >
-                Following
-            </button>
-        </header>
-    )
+      <button
+        onClick={() => setCurrentSection("Following")}
+        className={`header__tab ${
+          currentSection === "Following" ? "header__tab-active" : ""
+        }`}
+      >
+        Following
+      </button>
+    </header>
+  );
 }
