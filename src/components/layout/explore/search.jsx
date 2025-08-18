bbce14e0686d529e07fa39f97bb0ced87e9810e3
Leont1cov <chuchunovvakov@mail.ru>
2025-08-18 18:46:32 +0300

    feat: add layout explore page

---
+++ src/components/layout/explore/search.jsx
@@ -0,0 +1,9 @@
+export default function Search() {
+  return (
+    <div className="search">
+      <i className="search__icon fa fa-search" aria-hidden="true"></i>
+
+      <input className="search__input" type="text" placeholder="Search" />
+    </div>
+  );
+}
--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -1,3 +1,59 @@
+import { useState } from "react";
+import Search from "../components/layout/explore/search";
+import Header from "../components/layout/header/Header";
+import Sidebar from "../components/layout/sidebar/Sidebar";
+import InfoPanel from "../components/layout/info-panel/InfoPanel";
+
 export default function Explore() {
-  return <div></div>;
+  const [currentSection, setCurrentSection] = useState("For you");
+
+  return (
+    <main className="main">
+      <Sidebar />
+
+      <div className="main__center">
+        <Search />
+
+        <header className="header">
+          <button
+            onClick={() => setCurrentSection("For you")}
+            className={`header__tab ${
+              currentSection === "For you" ? "header__tab-active" : ""
+            }`}
+          >
+            For you
+          </button>
+
+          <button
+            onClick={() => setCurrentSection("Following")}
+            className={`header__tab ${
+              currentSection === "Following" ? "header__tab-active" : ""
+            }`}
+          >
+            Following
+          </button>
+
+          <button
+            onClick={() => setCurrentSection("News")}
+            className={`header__tab ${
+              currentSection === "News" ? "header__tab-active" : ""
+            }`}
+          >
+            News
+          </button>
+
+          <button
+            onClick={() => setCurrentSection("Sports")}
+            className={`header__tab ${
+              currentSection === "Sports" ? "header__tab-active" : ""
+            }`}
+          >
+            Sports
+          </button>
+        </header>
+      </div>
+
+      <InfoPanel />
+    </main>
+  );
 }
