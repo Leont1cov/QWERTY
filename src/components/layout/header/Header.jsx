ba5ec71eb54c03cfae5027cedde9e3054eff4d17
Rodion Kreimer <kreimerrodion@gmail.com>
2025-08-18 12:45:46 +0300

    fix: update the set current section state

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,27 +1,23 @@
-import { useState } from "react";
+export default function Header({ currentSection, setCurrentSection }) {
+    return (
+        <header className="header">
+            <button
+                onClick={() => setCurrentSection("For you")}
+                className={`header__tab ${
+                    currentSection === "For you" ? "header__tab-active" : ""
+                }`}
+            >
+                For you
+            </button>
 
-export default function Header() {
-  const [isActive, setIsActive] = useState("forYou");
-
-  return (
-    <header className="header">
-      <button
-        onClick={() => setIsActive("forYou")}
-        className={`header__tab ${
-          isActive === "forYou" ? "header__tab-active" : ""
-        }`}
-      >
-        For you
-      </button>
-
-      <button
-        onClick={() => setIsActive("following")}
-        className={`header__tab ${
-          isActive === "following" ? "header__tab-active" : ""
-        }`}
-      >
-        Following
-      </button>
-    </header>
-  );
+            <button
+                onClick={() => setCurrentSection("Following")}
+                className={`header__tab ${
+                    currentSection === "Following" ? "header__tab-active" : ""
+                }`}
+            >
+                Following
+            </button>
+        </header>
+    )
 }
