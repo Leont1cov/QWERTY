b6b0b1131087e4ed5a9bcc781e28f7d26a800e54
Leont1cov <chuchunovvakov@mail.ru>
2025-08-19 21:14:55 +0300

    feat: add saveLater component in Bookmaks page

---
+++ src/components/layout/bookmarks/saveLater.jsx
@@ -0,0 +1,10 @@
+export default function SaveLater() {
+  return (
+    <div className="saveLater">
+      <h1 className="saveLater__title">Save posts for later</h1>
+      <p className="saveLater__subtitle">
+        Bookmark posts to easily find them again in the future.
+      </p>
+    </div>
+  );
+}
